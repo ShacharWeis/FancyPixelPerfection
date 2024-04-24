6e0877ab1fcb922d5d06bbe8fd2b1022823900e5
ShacharWeis <vice@packet39.com>
2024-04-24 10:40:21 -0400

    Gama manager script

---
+++ Assets/FancyPixelPerfection/Scripts/PortalManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+
+    public void OpenPortals()
+    {
+        
+    }
+    public void ClosePortals()
+    {
+        
+    }
+}
